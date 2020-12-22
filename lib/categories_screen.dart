32941ad693716551e94e58f2f131d6fd7547f842
Ren Weilin <weilinwork99@gmail.com>
2020-12-22 21:19:18 +0800

    Styling for homepage and add nagivation

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -12,6 +12,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Deli Meal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
